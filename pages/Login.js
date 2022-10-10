2b1e13449c0e7d00f2a08a6635a74466e9c8baea
OverLord966 <daabea2001@gmail.com>
2022-10-10 12:13:44 +0100

    Added some commented code

--- pages/Login.js
+++ pages/Login.js
@@ -26,3 +26,44 @@ function Login(props) {
       )
   }
   export default Login
+
+
+  /*
+
+let user = "asd"
+    let pass = "dsa"
+    const handleAuthenticate = () => {
+          
+          if(user === authValues.username && pass === authValues.password){
+           alert("Olá " + user)
+            
+          }
+          else{
+            alert("Não foi encontrado nenhum user")
+          }
+    }
+    
+    return (
+    <div className={styles.titleLogin}>
+        <h3 className={styles.h3}>Log in</h3>
+        <div className={styles.login}>
+        <p className={styles.label}>Username:</p>
+            <input
+             
+                  type="text"
+                  value={authValues.username}
+                  onChange={(e) => setAuthValues(prevAuthValues => ({...prevAuthValues, username: e.target.value}))} />
+                  <p className={styles.label}>Password:</p>
+              <input
+                  type="password"
+                  value={authValues.password}
+                  onChange={(e) => setAuthValues(prevAuthValues => ({...prevAuthValues, password: e.target.value}))}  /><br></br>
+              <button type="submit" onClick={() => handleAuthenticate()}>Login</button>
+              <button type="button" onClick={() => alert("Hora de criar uma conta")}>SignUp</button>
+        </div>
+        
+</div>
+      )
+  }
+  export default Login
+  */
--- pages/index.js
+++ pages/index.js
@@ -39,3 +39,109 @@ export default function Home() {
     </div >
   )
 }
+
+
+/*
+return (
+    <div className={styles.container}>
+
+      <div className={styles.header}>
+        <div className={styles.headeritem}> 
+          <h1>Logo</h1>
+        </div>
+        <div className={styles.searchBar}>
+          <input type="text" placeholder="Search.." />
+          <SearchIcon />
+        </div>
+        <div className={styles.login}>
+          <a href={'http://localhost:3000/Login'} onClick={() => Login.Login}>Login</a>
+        </div>
+      </div>
+      <div className={styles.header}>
+        <div className={styles.retangulo}>
+
+        </div>
+      </div>
+      <div className={styles.body}>
+        
+        <div className={styles.squares}>
+          
+        </div>
+        <div className={styles.squares}>
+
+        </div>
+        <div className={styles.squares}>
+
+        </div>
+      </div>
+      <div className={styles.body}>
+        <div>
+          Preço: 999€<br></br>
+          Bunker: XXX
+        </div>
+        <div>
+          Preço: 999€<br></br>
+          Bunker: XXX
+        </div>
+        <div>
+          Preço: 999€<br></br>
+          Bunker: XXX
+        </div>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.squares}>
+
+        </div>
+        <div className={styles.squares}>
+
+        </div>
+        <div className={styles.squares}>
+
+        </div>
+        
+      </div>
+      <div className={styles.body}>
+        <div>
+          Preço: 999€<br></br>
+          Bunker: XXX
+        </div>
+        <div>
+          Preço: 999€<br></br>
+          Bunker: XXX
+        </div>
+        <div>
+          Preço: 999€<br></br>
+          Bunker: XXX
+        </div>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.squares}>
+
+        </div>
+        <div className={styles.squares}>
+
+        </div>
+        <div className={styles.squares}>
+
+        </div>
+        
+      </div>
+      <div className={styles.body}>
+        <div>
+          Preço: 999€<br></br>
+          Bunker: XXX
+        </div>
+        <div>
+          Preço: 999€<br></br>
+          Bunker: XXX
+        </div>
+        <div>
+          Preço: 999€<br></br>
+          Bunker: XXX
+        </div>
+      </div>
+    </div >
+  )
+
+
+*/

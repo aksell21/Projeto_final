1763e965d044b8af936abe1b8b41bb167faa0438
Miguel <miguel@Miguels-MacBook-Pro.local>
2022-10-10 18:35:50 +0100

    LOL

--- pages/Login.js
+++ pages/Login.js
@@ -1,69 +1,64 @@
-import {useState} from "react";
+import { useState } from "react";
 
 function Login(props) {
-    const [authValues, setAuthValues] = useState({username: "", password: ""})
-    
+    const [authValues, setAuthValues] = useState({ username: "", password: "" })
+
     const handleAuthenticate = () => {
-          props.setIsLoggedIn(
+        props.setIsLoggedIn(
             props.credenciais.find(credencial => (
-                credencial.username === authValues.username && 
+                credencial.username === authValues.username &&
                 credencial.password === authValues.password))?.username
-          )
+        )
     }
-    
+
     return (
-    <div className="">
-<input 
-                  type="text"
-                  value={authValues.username}
-                  onChange={(e) => setAuthValues(prevAuthValues => ({...prevAuthValues, username: e.target.value}))} />
-              <input
-                  type="password"
-                  value={authValues.password}
-                  onChange={(e) => setAuthValues(prevAuthValues => ({...prevAuthValues, password: e.target.value}))}  />
-              <button type="submit" onClick={() => handleAuthenticate()}>Login</button>
-</div>
-      )
-  }
-  export default Login
-
-
-  /*
-
-let user = "asd"
-    let pass = "dsa"
-    const handleAuthenticate = () => {
-          
-          if(user === authValues.username && pass === authValues.password){
-           alert("Olá " + user)
-            
-          }
-          else{
-            alert("Não foi encontrado nenhum user")
-          }
-    }
-    
-    return (
-    <div className={styles.titleLogin}>
-        <h3 className={styles.h3}>Log in</h3>
-        <div className={styles.login}>
-        <p className={styles.label}>Username:</p>
+        <div className="">
             <input
-             
-                  type="text"
-                  value={authValues.username}
-                  onChange={(e) => setAuthValues(prevAuthValues => ({...prevAuthValues, username: e.target.value}))} />
-                  <p className={styles.label}>Password:</p>
-              <input
-                  type="password"
-                  value={authValues.password}
-                  onChange={(e) => setAuthValues(prevAuthValues => ({...prevAuthValues, password: e.target.value}))}  /><br></br>
-              <button type="submit" onClick={() => handleAuthenticate()}>Login</button>
-              <button type="button" onClick={() => alert("Hora de criar uma conta")}>SignUp</button>
+                type="text"
+                value={authValues.username}
+                onChange={(e) => setAuthValues(prevAuthValues => ({ ...prevAuthValues, username: e.target.value }))} />
+            <input
+                type="password"
+                value={authValues.password}
+                onChange={(e) => setAuthValues(prevAuthValues => ({ ...prevAuthValues, password: e.target.value }))} />
+            <button type="submit" onClick={() => handleAuthenticate()}>Login</button>
         </div>
-        
-</div>
-      )
-  }
-  export default Login
-  */
+    )
+}
+
+
+
+
+// const handleAuthenticate = () => {
+
+//     if (user === authValues.username && pass === authValues.password) {
+//         alert("Olá " + user)
+
+//     }
+//     else {
+//         alert("Não foi encontrado nenhum user")
+//     }
+// }
+// return (
+//     <div className={styles.titleLogin}>
+//         <h3 className={styles.h3}>Log in</h3>
+//         <div className={styles.login}>
+//             <p className={styles.label}>Username:</p>
+//             <input
+
+//                 type="text"
+//                 value={authValues.username}
+//                 onChange={(e) => setAuthValues(prevAuthValues => ({ ...prevAuthValues, username: e.target.value }))} />
+//             <p className={styles.label}>Password:</p>
+//             <input
+//                 type="password"
+//                 value={authValues.password}
+//                 onChange={(e) => setAuthValues(prevAuthValues => ({ ...prevAuthValues, password: e.target.value }))} /><br></br>
+//             <button type="submit" onClick={() => handleAuthenticate()}>Login</button>
+//             <button type="button" onClick={() => alert("Hora de criar uma conta")}>SignUp</button>
+//         </div>
+
+//     </div>
+// )
+//   }
+export default Login
--- pages/index.js
+++ pages/index.js
@@ -2,146 +2,216 @@ import { useState } from 'react'
 import Login from './Login'
 import styles from '../styles/HomePage.module.css'
 import SearchIcon from '@mui/icons-material/Search';
+import Link from 'next/link';
 
 
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
+  
+return (
+  <div className={styles.container}>
 
-      <div className={styles.header}>
-        <div className={styles.headeritem}> <h1>Logo</h1></div>
-        <div className={styles.headeritem}>
+    <div className={styles.header}>
+      <div className={styles.headeritem}> 
+        <h1>Logo</h1>
+      </div>
+      <div className={styles.searchBar}>
+        <div className={styles.search}>
           <input type="text" placeholder="Search.." />
           <SearchIcon />
         </div>
-        <div className={styles.headeritem}>
-
-        </div>
       </div>
-      <div className={styles.body}>
-        <div className={styles.squares}>
-
-        </div>
-        <div className={styles.squares}>
-
-        </div>
-        <div className={styles.squares}>
-
-        </div>
-        <div className={styles.squares}>
-
-        </div>
-        <div className={styles.squares}>
-
-        </div>
+      <div className={styles.login}>
+        <Link href={'/Login'}>Login</Link>
       </div>
-    </div >
+    </div>
+    <div className={styles.header}>
+      <div className={styles.retangulo}>
+
+      </div>
+    </div>
+    <div className={styles.body}>
+      
+      <div className={styles.squares}>
+        
+      </div>
+      <div className={styles.squares}>
+
+      </div>
+      <div className={styles.squares}>
+
+      </div>
+    </div>
+    <div className={styles.body}>
+      <div>
+        Preço: 999€<br></br>
+        Bunker: XXX
+      </div>
+      <div>
+        Preço: 999€<br></br>
+        Bunker: XXX
+      </div>
+      <div>
+        Preço: 999€<br></br>
+        Bunker: XXX
+      </div>
+    </div>
+    <div className={styles.body}>
+      <div className={styles.squares}>
+
+      </div>
+      <div className={styles.squares}>
+
+      </div>
+      <div className={styles.squares}>
+
+      </div>
+      
+    </div>
+    <div className={styles.body}>
+      <div>
+        Preço: 999€<br></br>
+        Bunker: XXX
+      </div>
+      <div>
+        Preço: 999€<br></br>
+        Bunker: XXX
+      </div>
+      <div>
+        Preço: 999€<br></br>
+        Bunker: XXX
+      </div>
+    </div>
+    <div className={styles.body}>
+      <div className={styles.squares}>
+
+      </div>
+      <div className={styles.squares}>
+
+      </div>
+      <div className={styles.squares}>
+
+      </div>
+      
+    </div>
+    <div className={styles.body}>
+      <div>
+        Preço: 999€<br></br>
+        Bunker: XXX
+      </div>
+      <div>
+        Preço: 999€<br></br>
+        Bunker: XXX
+      </div>
+      <div>
+        Preço: 999€<br></br>
+        Bunker: XXX
+      </div>
+    </div>
+  </div >
+
   )
 }
 
 
-/*
-return (
-    <div className={styles.container}>
 
-      <div className={styles.header}>
-        <div className={styles.headeritem}> 
-          <h1>Logo</h1>
-        </div>
-        <div className={styles.searchBar}>
-          <input type="text" placeholder="Search.." />
-          <SearchIcon />
-        </div>
-        <div className={styles.login}>
-          <a href={'http://localhost:3000/Login'} onClick={() => Login.Login}>Login</a>
-        </div>
-      </div>
-      <div className={styles.header}>
-        <div className={styles.retangulo}>
+// return (
+//     <div className={styles.container}>
 
-        </div>
-      </div>
-      <div className={styles.body}>
+//       <div className={styles.header}>
+//         <div className={styles.headeritem}> 
+//           <h1>Logo</h1>
+//         </div>
+//         <div className={styles.searchBar}>
+//           <input type="text" placeholder="Search.." />
+//           <SearchIcon />
+//         </div>
+//         <div className={styles.login}>
+//           <a href={'http://localhost:3000/Login'} onClick={() => Login.Login}>Login</a>
+//         </div>
+//       </div>
+//       <div className={styles.header}>
+//         <div className={styles.retangulo}>
+
+//         </div>
+//       </div>
+//       <div className={styles.body}>
         
-        <div className={styles.squares}>
+//         <div className={styles.squares}>
           
-        </div>
-        <div className={styles.squares}>
+//         </div>
+//         <div className={styles.squares}>
 
-        </div>
-        <div className={styles.squares}>
+//         </div>
+//         <div className={styles.squares}>
 
-        </div>
-      </div>
-      <div className={styles.body}>
-        <div>
-          Preço: 999€<br></br>
-          Bunker: XXX
-        </div>
-        <div>
-          Preço: 999€<br></br>
-          Bunker: XXX
-        </div>
-        <div>
-          Preço: 999€<br></br>
-          Bunker: XXX
-        </div>
-      </div>
-      <div className={styles.body}>
-        <div className={styles.squares}>
+//         </div>
+//       </div>
+//       <div className={styles.body}>
+//         <div>
+//           Preço: 999€<br></br>
+//           Bunker: XXX
+//         </div>
+//         <div>
+//           Preço: 999€<br></br>
+//           Bunker: XXX
+//         </div>
+//         <div>
+//           Preço: 999€<br></br>
+//           Bunker: XXX
+//         </div>
+//       </div>
+//       <div className={styles.body}>
+//         <div className={styles.squares}>
 
-        </div>
-        <div className={styles.squares}>
+//         </div>
+//         <div className={styles.squares}>
 
-        </div>
-        <div className={styles.squares}>
+//         </div>
+//         <div className={styles.squares}>
 
-        </div>
+//         </div>
         
-      </div>
-      <div className={styles.body}>
-        <div>
-          Preço: 999€<br></br>
-          Bunker: XXX
-        </div>
-        <div>
-          Preço: 999€<br></br>
-          Bunker: XXX
-        </div>
-        <div>
-          Preço: 999€<br></br>
-          Bunker: XXX
-        </div>
-      </div>
-      <div className={styles.body}>
-        <div className={styles.squares}>
+//       </div>
+//       <div className={styles.body}>
+//         <div>
+//           Preço: 999€<br></br>
+//           Bunker: XXX
+//         </div>
+//         <div>
+//           Preço: 999€<br></br>
+//           Bunker: XXX
+//         </div>
+//         <div>
+//           Preço: 999€<br></br>
+//           Bunker: XXX
+//         </div>
+//       </div>
+//       <div className={styles.body}>
+//         <div className={styles.squares}>
 
-        </div>
-        <div className={styles.squares}>
+//         </div>
+//         <div className={styles.squares}>
 
-        </div>
-        <div className={styles.squares}>
+//         </div>
+//         <div className={styles.squares}>
 
-        </div>
+//         </div>
         
-      </div>
-      <div className={styles.body}>
-        <div>
-          Preço: 999€<br></br>
-          Bunker: XXX
-        </div>
-        <div>
-          Preço: 999€<br></br>
-          Bunker: XXX
-        </div>
-        <div>
-          Preço: 999€<br></br>
-          Bunker: XXX
-        </div>
-      </div>
-    </div >
-  )
-
-
-*/
+//       </div>
+//       <div className={styles.body}>
+//         <div>
+//           Preço: 999€<br></br>
+//           Bunker: XXX
+//         </div>
+//         <div>
+//           Preço: 999€<br></br>
+//           Bunker: XXX
+//         </div>
+//         <div>
+//           Preço: 999€<br></br>
+//           Bunker: XXX
+//         </div>
+//       </div>
+//     </div >
+// )

86508b260e0c762a89c72ec39776050afb9f8f16
Miguel <miguel@Miguels-MacBook-Pro.local>
2022-10-12 12:19:21 +0100

    frontend alterado

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,14 @@ import SearchIcon from '@mui/icons-material/Search';
 import Link from 'next/link';
 
 
-
-
-export default function Home() {
-  
-return (
-  <div className={styles.container}>
-
-    <div className={styles.header}>
-      <div className={styles.headeritem}> 
-        <h1>Logo</h1>
+function Header(){
+  return (
+    <div className={styles.header}
+    style = {{backgroundImage:"url(/header.jpg)"}}>
+      <h1>LOOKING TO SURVIVE IN STYLE?</h1>
+      <h5>A.BUNKER has the best solutions to enjoy the end of days.</h5>
+      <div className={styles.headeritem}>
+        <h2>Logo</h2>
       </div>
 
       <div className={styles.searchBar}>
@@ -29,18 +27,24 @@ return (
        
       
       <div className={styles.login}> 
-       <Link href={'/criarbunker'}><button className={styles.button}>Criar Bunker</button></Link>
         <Link href={'/Login'}><button className={styles.button_login}>Login</button></Link>
-        <Link href={'/Singup'}><button className={styles.button_singup}>SingUp</button></Link>
+        <Link href={'/Singup'}><button className={styles.button_singup}>Sign Up</button></Link>
       </div>
 
     </div>
-    <div className={styles.header}>
-      <div className={styles.retangulo}>
+  )
+}
 
-      </div>
-    </div>
+export default function Home() {
+  
+return (
+  <div className={styles.container}>
+<Header></Header>
+    
+    <div><h2>Secure your own private bunker.</h2>
+      <h5>Our selection.</h5></div>
     <div className={styles.body}>
+      
       
       <div className={styles.squares}>
         
@@ -53,17 +57,51 @@ return (
       </div>
     </div>
     <div className={styles.body}>
+      
       <div>
-        Preço: 999€<br></br>
-        Bunker: XXX
+        Preço: 2.2 M€<br></br>
+        Bunker: VIVOS EUROPA ONE<br></br>
+        Rohenstein, Germany
       </div>
       <div>
-        Preço: 999€<br></br>
-        Bunker: XXX
+        Preço: 18 M€<br></br>
+        Bunker: LAS VEGAS UNDERGROUND<br></br>
+        Las Vegas, United States
       </div>
       <div>
-        Preço: 999€<br></br>
-        Bunker: XXX
+        Preço: 3 M€<br></br>
+        Bunker: THE SAFE HOUSE<br></br>
+        Warsaw, Poland
+      </div>
+    </div>
+    <div className={styles.body}>
+      <div className={styles.squares}>
+
+      </div>
+      <div className={styles.squares}>
+
+      </div>
+      <div className={styles.squares}>
+
+      </div>
+      
+    </div>
+    <div className={styles.body}>
+  
+      <div>
+        Preço: 4.8 M€<br></br>
+        Bunker: HOUSE PLAN B<br></br>
+        Ukraine
+      </div>
+      <div>
+        Preço: 500 000€<br></br>
+        Bunker: ARTEMIS PROTECTION<br></br>
+        Nantes, France
+      </div>
+      <div>
+        Preço: 8 M€<br></br>
+        Bunker: THE OPPIDUM<br></br>
+        Czech Republic
       </div>
     </div>
     <div className={styles.body}>
@@ -80,46 +118,38 @@ return (
     </div>
     <div className={styles.body}>
       <div>
-        Preço: 999€<br></br>
-        Bunker: XXX
+        Preço: 40 000 €<br></br>
+        Bunker: VIVOS XPOINT<br></br>
+        South Dakota, United States
       </div>
       <div>
-        Preço: 999€<br></br>
-        Bunker: XXX
+        Preço: 5.4M €<br></br>
+        Bunker: DBX DOOMSDAY BUNKER<br></br>
+        South Carolina, United States
       </div>
       <div>
-        Preço: 999€<br></br>
-        Bunker: XXX
-      </div>
-    </div>
-    <div className={styles.body}>
-      <div className={styles.squares}>
-
-      </div>
-      <div className={styles.squares}>
-
-      </div>
-      <div className={styles.squares}>
-
-      </div>
-      
-    </div>
-    <div className={styles.body}>
-      <div>
-        Preço: 999€<br></br>
-        Bunker: XXX
-      </div>
-      <div>
-        Preço: 999€<br></br>
-        Bunker: XXX
-      </div>
-      <div>
-        Preço: 999€<br></br>
-        Bunker: XXX
+        Preço: 600 000 €<br></br>
+        Bunker: NOMAD BUNKER<br></br>
+        Anywhere
       </div>
     </div>
   </div >
+  
 
+  )
+}
+function A() {
+  return (
+      <div>
+          <div className={styles.upper}>
+              <img></img>
+              <div></div>
+          </div>
+          <div className={styles.lower}>
+              <div></div>
+              <div></div>
+          </div>
+      </div>
   )
 }
 
